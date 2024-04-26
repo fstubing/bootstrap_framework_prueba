62b52e87f37e4e08a40c92c9bc4b0cba28ed2ae0
fernando stubing <fstubing@yahoo.com>
2024-04-26 19:28:48 -0400

    subida final

---
+++ assets/js/index.js
@@ -0,0 +1,34 @@
+import "../../node_modules/bootstrap/dist/js/bootstrap.bundle.js";
+
+// lógica de tooltip bootstrap
+const tooltipTriggerList = document.querySelectorAll(
+  '[data-bs-toggle="tooltip"]'
+);
+const tooltipList = [...tooltipTriggerList].map(
+  (tooltipTriggerEl) => new bootstrap.Tooltip(tooltipTriggerEl)
+);
+
+// lógica de alerta
+const alertPlaceholder = document.getElementById("liveAlertPlaceholder");
+const appendAlert = (message, type) => {
+  const wrapper = document.createElement("div");
+  wrapper.innerHTML = [
+    `<div class="alert alert-${type} alert-dismissible" role="alert">`,
+    `   <div>${message}</div>`,
+    '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
+    "</div>",
+  ].join("");
+
+  alertPlaceholder.append(wrapper);
+};
+
+const alertTrigger = document.getElementById("liveAlertBtn");
+if (alertTrigger) {
+  alertTrigger.addEventListener("click", (e) => {
+    e.preventDefault();
+    appendAlert(
+      "Tu mensaje fue enviado con éxito. Nos contactarémos contigo pronto!",
+      "success"
+    );
+  });
+}
